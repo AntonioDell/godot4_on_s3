d4e750debb3a22b8fa013d7105c55e9a40cba48a
Antonio Dell'Annunziata <contact@antonio-da.dev>
2022-12-03 10:47:55 +0100

    Implement guide step "Changes to Godot game"

--- config.gd
+++ config.gd
@@ -1,3 +1,14 @@
 extends Node
 
-var base_url: String = "https://s3.eu-central-1.amazonaws.com/godot4-on-s3.xyz"
+var base_url: String = ""
+
+func _init():
+	for argument in OS.get_cmdline_args():
+		if argument.find("=") > -1:
+			var key_value = argument.split("=")
+			if "data-bucket-url" in key_value[0]:
+				base_url = key_value[1]
+	if base_url.is_empty():
+		printerr("No command line argument for base url found! Provided arguments were: %s" % OS.get_cmdline_args())
+	else:
+		print("Using '%s' as base url." % base_url)
